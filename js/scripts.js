c9674b74a6f2474d2bb9bf1632f9faebc749d44d
naklpakl <119118058+naklpakl@users.noreply.github.com>
2022-11-26 17:13:38 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+// Custom Scripts
+// Custom scripts
+function stepForm() {
+    const steps = document.querySelectorAll('.form__step')
+    const prevBtn = document.querySelector('.prev__step')
+    const nextBtn = document.querySelector('.next__step')
+    const form = document.querySelector('.steps__form')
+    const stepNumbers = document.querySelectorAll('.step__number')
+    const progress = document.querySelector('.progress__success')
+    const finishBlock = document.querySelector('.finish')
+
+    form.addEventListener('submit', e => e.preventDefault())
+
+    let formStepIndex = 0
+
+    prevBtn.addEventListener('click', () => {
+        formStepIndex--
+
+        stepNumbers[formStepIndex + 1].classList.remove('active__number')
+
+        updateformStep()
+    })
+
+
+    nextBtn.addEventListener('click', () => {
+        if (formStepIndex < steps.length - 1) {
+            formStepIndex++
+            updateformStep()
+        }
+    })
+
+
+    function updateformStep() {
+        steps.forEach(step => {
+            step.classList.contains('active') && step.classList.remove('active')
+        })
+
+        steps[formStepIndex].classList.add('active')
+
+        stepNumbers[formStepIndex].classList.add('active__number')
+
+        if (formStepIndex == 0) {
+            prevBtn.style.display = 'none'
+        } else {
+            prevBtn.style.display = 'block'
+        }
+        
+        if (formStepIndex == steps.length - 1) {
+            nextBtn.innerHTML = "Finish"
+
+            nextBtn.addEventListener('click', () => {
+                finishBlock.style.display = 'block'
+                form.style.display = 'none '
+            })
+        } else {
+            nextBtn.innerHTML = "Next"
+        }
+        const actives = document.querySelectorAll('.active__number')
+        const percent = (( actives.length - 1) / (stepNumbers.length - 1)) * 100 + '%'
+        progress.style.width = percent
+    }
+
+    updateformStep()
+
+
+
+
+}
+
+if(document.querySelector('.form__step')) {
+    stepForm()
+}
+
